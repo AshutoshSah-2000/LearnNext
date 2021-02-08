0143da323fe4cdb0fdb866aad35a1ab9aa696f12
AshutoshSah-2000 <42838241+AshutoshSah-2000@users.noreply.github.com>
2021-02-08 12:16:29 +0530

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,11 @@
+import Image from 'next/image';
+
 export default function about() {
     return (
         <div>
             <h1 >About Page</h1>
         <h4>Checking if update works!</h4>
+        <Image src='/mit.webp'></Image>
         </div>
     )
 }
