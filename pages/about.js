392e12f210a80f42862e89879140610dbb34a9ea
AshutoshSah-2000 <42838241+AshutoshSah-2000@users.noreply.github.com>
2021-02-08 18:28:29 +0530

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,10 @@
+import Image from 'next/image';
 export default function about() {
     return (
         <div>
             <h1 >About Page</h1>
         <h4>Checking if update works!</h4>
-        <img src='/LearnNext/mit.webp' alt = 'imagesdf'></img>
+        <Image src='/LearnNext/mit.webp' alt = 'imagesdf'></Image>
         </div>
     )
 }
