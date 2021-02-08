58c31ab684952148fff38d4ca0775656d37db136
AshutoshSah-2000 <42838241+AshutoshSah-2000@users.noreply.github.com>
2021-02-08 13:10:07 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,19 +1,7 @@
-const withPlugins = require('next-compose-plugins');
-const optimizedImages = require('next-optimized-images');
 
-module.exports = withPlugins([
-  [optimizedImages, {
-    mozjpeg: {
-      quality: 80,
-    },
-    pngquant: {
-      speed: 3,
-      strip: true,
-      verbose: true,
-    },
-    imagesPublicPath: '/LearnNext/_next/static/images/',
-  }],{
+module.exports ={
+  images: {
+    loader: 'imgix'},
     assetPrefix: '/LearnNext/',
     basePath: '/LearnNext'
-},
-    ]);
+}   
